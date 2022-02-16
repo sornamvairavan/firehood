a08d84fd0c3030e4c0410be033e716a35b041bc9
Sornam Vairavan <sornamvairavan@outlook.com>
2022-02-16 09:33:19 -0800

    tried updating home-logo

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -3,7 +3,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Link, NavLink } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
 import { login } from '../../store/session';
+import homelogo from '../../images/firehood-logo.png'
 import './NavBar.css'
+
 
 const NavBar = () => {
   const dispatch = useDispatch()
@@ -34,7 +36,7 @@ const NavBar = () => {
     <div className="nav-bar">
         <span className="left-nav-bar">
           <NavLink to='/' exact={true} >
-            <img src="/images/firehood-logo.png" alt="home" className="logo-img"/>
+            <img src={homelogo} alt="home" className="logo-img"/>
           </NavLink>
         </span>
         <span className="right-nav-bar">
