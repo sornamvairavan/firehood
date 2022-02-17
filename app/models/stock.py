a7254169c9504cbbeadcbe92b78a9c41c4fa8dc0
Sornam Vairavan <sornamvairavan@outlook.com>
2022-02-17 08:27:09 -0800

    updated stock model

--- app/models/stock.py
+++ app/models/stock.py
@@ -8,7 +8,7 @@ class Stock(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     company_name = db.Column(db.String(255), nullable=False)
     ticker_symbol = db.Column(db.String(20), nullable=False)
-    # price = db.Column(db.Float)
+    price = db.Column(db.Float)
 
     portfolio = db.relationship("Portfolio", back_populates="stock")
     transaction = db.relationship("Transaction", back_populates="stock")
@@ -19,5 +19,5 @@ class Stock(db.Model):
             'id': self.id,
             'company_name': self.company_name,
             'ticker': self.ticker_symbol,
-            # 'price': self.price
+            'price': self.price
         }
