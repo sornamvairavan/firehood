0422bfef57b7c904f3c90897be7bbad79878bf39
Sornam Vairavan <sornamvairavan@outlook.com>
2022-02-18 00:14:20 -0800

    updated active nav-links

--- react-app/src/components/Transactions/index.js
+++ react-app/src/components/Transactions/index.js
@@ -6,13 +6,45 @@ import { useDispatch, useSelector } from 'react-redux';
 export default function Transaction() {
     const dispatch = useDispatch()
 
+    const userTransactionsObj = useSelector(state => state.transaction.transactions)
+    const userTransactionsArr = Object.values(userTransactionsObj)
+    const [isLoaded, setIsLoaded] = useState(false);
+
     useEffect(() => {
         dispatch(getUserTransactions())
-    }, [dispatch])
+            .then(() => setIsLoaded(true))
+    }, [dispatch, isLoaded])
 
     return (
         <div>
             <h4>Transactions</h4>
+            {/* {userTransactionsArr.length === 0 && (
+                <h2>You have no transactions</h2>
+            )} */}
+            {userTransactionsArr.length > 0 && (
+            <table>
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Type</th>
+                        <th>Stock</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {(userTransactionsArr.map((transaction, idx) => (
+                    <tr key={idx}>
+                        <td>{transaction?.created_at}</td>
+                        <td>{transaction?.type}</td>
+                        <td>{transaction?.stock?.ticker}</td>
+                        <td>$price</td>
+                        <td>{transaction?.quantity}</td>
+                    </tr>
+                )))}
+                </tbody>
+            </table>
+            )}
         </div>
     )
 }
