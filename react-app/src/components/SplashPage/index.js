e4095ba75e36b6754210dfc337149eee37071f75
Sornam Vairavan <sornamvairavan@outlook.com>
2022-02-18 21:54:28 -0800

    updated splash page

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -1,5 +1,5 @@
 import { useSelector } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import logo from '../../images/logo.png'
 import './SplashPage.css'
 
@@ -12,25 +12,31 @@ export default function SplashPage() {
   }
 
     return (
-      <div className="splash-container">
-        <div className='logo'>
-          <img src={logo} alt="logo" className="splash-logo"/>
+      <div className='container'>
+        <div className="splash-container">
+          <div className="splash-text-container">
+            <h2 className="header1">Investing for Everyone</h2>
+            <div>
+              <h3 className="one-liner">Your way to Financial Independence, Retire Early (FIRE)</h3>
+              <h3 className="one-liner">Commission-free investing, plus the tools you need to put your money in motion.</h3>
+            </div>
+            <div className="signup-cont">
+              <Link to="/signup" className='splash-signup'>Sign up</Link>
+            </div>
+          </div>
+          <div className='logo'>
+            <img src={logo} alt="logo" className="splash-logo"/>
+          </div>
         </div>
-        <div className="splash-text-container">
-          <h2 className="header1">Investing for Everyone</h2>
-          <h5 className="one-liner">Your way to Financial Independence, Retire Early (FIRE)</h5>
-          <h5 className="one-liner">Commission-free investing, plus the tools you need to put your money in motion.</h5>
-
-        </div>
-        {/* <footer className='network-links'>
-          <div>Sornam Vairavan</div>
-          <a href="https://www.linkedin.com/in/sornamvairavan" target="_blank" rel="noreferrer">
-            <i className="fab fa-linkedin fa-lg"></i>
-          </a>
-          <a href="https://github.com/sornamvairavan" target="_blank" rel="noreferrer">
-            <i className="fab fa-github fa-lg"></i>
-          </a>
-        </footer> */}
+          <footer className='network-links'>
+            <div>Sornam Vairavan</div>
+            <a href="https://www.linkedin.com/in/sornamvairavan" target="_blank" rel="noreferrer">
+              <i className="fab fa-linkedin fa-lg"></i>
+            </a>
+            <a href="https://github.com/sornamvairavan" target="_blank" rel="noreferrer">
+              <i className="fab fa-github fa-lg"></i>
+            </a>
+          </footer>
       </div>
     )
   }
