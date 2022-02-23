b2a5c103c851b7508e948fa787e7d9b4e5e83958
Sornam Vairavan <sornamvairavan@outlook.com>
2022-02-23 08:22:25 -0800

    added created at for transactions

--- app/seeds/transactions.py
+++ app/seeds/transactions.py
@@ -2,9 +2,9 @@ from app.models import db, Transaction
 
 def seed_transactions():
     demo_t1 = Transaction(
-        type="Buy", price=386.67,  quantity=5, user_id=1, stock_id=68)
+        type="Buy", price=386.67,  quantity=5, user_id=1, stock_id=68, created_at="2022-02-18 08:13:37.516563-08")
     demo_t2 = Transaction(
-        type="Buy", price=207.71,  quantity=10, user_id=1, stock_id=39)
+        type="Buy", price=207.71,  quantity=10, user_id=1, stock_id=39, created_at="2022-02-18 08:13:37.516563-08")
 
     db.session.add(demo_t1)
     db.session.add(demo_t2)
