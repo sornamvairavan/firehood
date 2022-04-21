4849d56d8c810ca4067497828c4117e52dae36e8
Sornam Vairavan <sornamvairavan@outlook.com>
2022-04-21 08:33:02 -0700

    updated portfolio seeders

--- app/seeds/portfolios.py
+++ app/seeds/portfolios.py
@@ -2,9 +2,9 @@ from app.models import db, Portfolio
 
 def seed_portfolios():
     demo_p1 = Portfolio(
-        price=213.22,  quantity=5, user_id=1, stock_id=329)
+        price=213.78,  quantity=5, user_id=1, stock_id=329)
     demo_p2 = Portfolio(
-        price=194.25,  quantity=10, user_id=1, stock_id=190)
+        price=193.23,  quantity=10, user_id=1, stock_id=190)
 
     db.session.add(demo_p1)
     db.session.add(demo_p2)
